70d0a5dedc64c761118f2455dbce71a7187bc4e2
Nadine Amr <69207331+NaddinA@users.noreply.github.com>
2021-06-21 23:29:44 +0200

    Add files via upload

---
+++ Hacker.cs
@@ -0,0 +1,169 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    //Game configuration
+    string[] level1Pass = { "books", "aisle", "shelf", "password", "font", "borrow" };
+    string[] level2Pass = { "prisoner", "handcuffs", "arrest", "holster", "heist" };
+    string[] level3Pass = { "starfield", "telescope", "exploration", "comet", "astronauts" };
+
+
+
+    //game state
+    int level;
+    enum Screen { MainMenu, Password, Win };
+    Screen currentScreen;
+    string password;
+
+    //hints
+    const string menuHint = "You can type menu at any time.";
+
+    void Start()
+    {
+        ShowMainMenu();
+    }
+    void ShowMainMenu()
+    {
+        currentScreen = Screen.MainMenu;
+        Terminal.ClearScreen();
+        Terminal.WriteLine("What would you like to hack into?");
+        Terminal.WriteLine("Press 1 for the local library");
+        Terminal.WriteLine("Press 2 for the police station");
+        Terminal.WriteLine("press 3 for NASA");
+        Terminal.WriteLine("Enter your selection: ");
+    }
+    void OnUserInput(string input)
+    {
+        if (input == "menu") // we can always go direct to main menu
+        {
+            ShowMainMenu();
+        }
+        else if (input == "quit" || input == "close" || input == "exit")
+        {
+            Terminal.WriteLine("Please close tab if you're on the web.");
+            Application.Quit();
+        }
+        else if (currentScreen == Screen.MainMenu)
+        {
+            RunMainMenu(input);
+        }
+        else if (currentScreen == Screen.Password)
+        {
+            CheckPassword(input);
+        }
+    }
+
+    void RunMainMenu(string input)
+    {
+        bool isValidlevel = (input == "1" || input == "2" || input == "3");
+        if (isValidlevel) {
+            level = int.Parse(input); //change input from string to int
+            AskForPassword();
+        }
+
+        else if (input == "007")
+        {
+            Terminal.WriteLine("Please select a level Mr Bond!");
+        }
+        else
+        {
+            Terminal.WriteLine("Please choose a valid level");
+            Terminal.WriteLine(menuHint);
+        }
+    }
+    void AskForPassword()
+    {
+        currentScreen = Screen.Password;
+        Terminal.ClearScreen();
+        SetRandomPassword();
+        Terminal.WriteLine("Enter your password, hint: " + password.Anagram());
+        Terminal.WriteLine(menuHint);
+    }
+
+    void SetRandomPassword()
+    {
+        switch (level)
+        {
+            case 1:
+                password = level1Pass[Random.Range(0, level1Pass.Length)];
+                break;
+            case 2:
+                password = level2Pass[Random.Range(0, level2Pass.Length)];
+                break;
+            case 3:
+                password = level3Pass[Random.Range(0, level3Pass.Length)];
+                break;
+            default:
+                Debug.LogError("invalid level number");
+                break;
+        }
+    }
+
+    void CheckPassword(string input)
+    {
+        if (input == password)
+        {
+            DisplayWinScreen();
+        }
+        else
+        {
+            AskForPassword();
+        }
+    }
+
+    void DisplayWinScreen()
+    {
+        currentScreen = Screen.Win;
+        Terminal.ClearScreen();
+        ShowLevelReward();
+        Terminal.WriteLine(menuHint);
+    }
+
+    void ShowLevelReward()
+    {
+        switch (level)
+        {
+            case 1:
+                Terminal.WriteLine("You Win! uwu");
+                Terminal.WriteLine(@"
+──────▄▀▄─────▄▀▄
+─────▄█░░▀▀▀▀▀░░█▄
+─▄▄──█░░░░░░░░░░░█──▄▄
+█▄▄█─█░░▀░░┬░░▀░░█─█▄▄█
+");
+                break;
+
+            case 2:
+                Terminal.WriteLine("You win again! :D");
+                Terminal.WriteLine("Play again for a greater challenge.");
+                Terminal.WriteLine(@"
+─▄▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▄
+█░░░█░░░░░░░░░░▄▄░██░█
+█░▀▀█▀▀░▄▀░▄▀░░▀▀░▄▄░█
+█░░░▀░░░▄▄▄▄▄░░██░▀▀░█
+─▀▄▄▄▄▄▀─────▀▄▄▄▄▄▄▀
+");
+                
+                break;
+
+            case 3:
+                Terminal.WriteLine(@"
+ _ __   __ _ ___  __ _
+| '_ \ / _` / __|/ _` |
+| | | | (_| \__ \ (_| |
+|_| |_|\__,_|___)\__,_|
+
+");
+                Terminal.WriteLine("Welcome to NASA's internal system!");
+                Terminal.WriteLine("Shoutout to Mr.Ben Tristem for this art♡");
+                break;
+
+            default:
+                Debug.LogError("Invalid level reached.");
+                break;
+        }
+
+    }
+}
